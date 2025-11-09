948a3f47ec07d4ae59d32034f5ad5a4f85434178
kite1412 <frozradit@gmail.com>
2025-11-09 19:20:23 +0700

    feat: add nav bar protection

--- app/src/main/java/kite1412/irrigo/MainActivity.kt
+++ app/src/main/java/kite1412/irrigo/MainActivity.kt
@@ -4,8 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import dagger.hilt.android.AndroidEntryPoint
 import kite1412.irrigo.designsystem.theme.IrrigoTheme
@@ -18,16 +29,38 @@ class MainActivity : ComponentActivity() {
         setContent {
             IrrigoTheme { innerPadding ->
                 SplashScreen {
-                    IrrigoNavHost(
-                        modifier = Modifier
-                            .padding(innerPadding)
-                            .padding(
-                                vertical = 24.dp,
-                                horizontal = 16.dp
-                            )
-                    )
+                    Box(
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        IrrigoNavHost(
+                            modifier = Modifier
+                                .padding(innerPadding)
+                                .padding(
+                                    vertical = 24.dp,
+                                    horizontal = 16.dp
+                                )
+                        )
+                        NavigationBarProtection(
+                            modifier = Modifier.align(Alignment.BottomCenter)
+                        )
+                    }
                 }
             }
         }
+    }
+
+    @Composable
+    private fun NavigationBarProtection(modifier: Modifier = Modifier) {
+        Box(
+            modifier = modifier
+                .fillMaxWidth()
+                .height(
+                    WindowInsets
+                        .navigationBars
+                        .asPaddingValues()
+                        .calculateBottomPadding()
+                )
+                .background(Color.Black.copy(alpha = 0.3f))
+        )
     }
 }
